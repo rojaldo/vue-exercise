04d32ae339283506b2296602d26addd5171dbf04
Daniel Rojo <danielrojo@gmail.com>
2022-02-22 18:34:39 +0100

    heroes component list and form

--- src/components/heroes/HeroesComponent.vue
+++ src/components/heroes/HeroesComponent.vue
@@ -1,12 +1,32 @@
 <template>
 <div class="container">
-    <h1>this is heroes component!</h1>
+    <div class="form-group">
+      <label for="">Hero Name</label>
+      <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="" v-model="heroName">
+    </div>
+    <button type="button" class="btn btn-primary mt-3" @click="addHero()" :disabled="heroName.length === 0">Add Hero</button>
+    <hr class="mb-5">
+    <ul class="list-group">
+        <li class="list-group-item" v-for="(hero, index) in heroes" :key="index">{{hero}}</li>
+    </ul>
 </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'HeroesComponent',
+        data() {
+            return {
+                heroName: '',
+                heroes: ['Batman', 'Superman', 'Spiderman'],
+            };
+        },
+        methods: {
+            addHero() {
+                this.heroes.push(this.heroName);
+                this.heroName = '';
+            },
+        },
     }
 </script>
 
