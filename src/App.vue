779fd8e4533ef15e509945f6928740ccbe2b426d
Daniel Rojo <danielrojo@gmail.com>
2022-02-23 19:43:04 +0100

    Beer component added

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <b-tabs content-class="mt-3">
       <b-tab title="Calculator" ><CalculatorComponent></CalculatorComponent></b-tab>
       <b-tab title="Heroes" ><HeroesComponent></HeroesComponent></b-tab>
-      <b-tab title="Apod" :active="true"><ApodComponent></ApodComponent></b-tab>
+      <b-tab title="Apod"><ApodComponent></ApodComponent></b-tab>
+      <b-tab title="Beers" :active="true"><BeersComponent></BeersComponent></b-tab>
     </b-tabs>
     
   </div>
@@ -13,6 +14,7 @@
 import CalculatorComponent from "./components/calculator/CalculatorComponent.vue";
 import HeroesComponent from "./components/heroes/HeroesComponent.vue";
 import ApodComponent from "./components/apod/ApodComponent.vue";
+import BeersComponent from "./components/beers/BeersComponent.vue";
 
 export default {
   name: "App",
@@ -20,6 +22,7 @@ export default {
     CalculatorComponent,
     HeroesComponent,
     ApodComponent,
+    BeersComponent
   },
 };
 </script>
---
+++ src/components/beers/BeersComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-sm-12 col-md-6 col-lg-4 my-2"
+        v-for="(beer, index) in beers"
+        :key="index"
+      >
+        <div class="card h-100">
+          <img class="card-img-top mx-auto mt-2" :src="beer.image_url" alt="" style="width: 60px" />
+          <div class="card-body">
+            <h4 class="card-title">{{ beer.name }}</h4>
+            <h6 >{{ beer.first_brewed }}</h6>
+            <h6 >{{ beer.abv }}</h6>
+            <p class="card-text">{{ beer.tagline }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BeersComponent",
+  data() {
+    return {
+      beers: [],
+    };
+  },
+  created() {
+    this.getBeers();
+  },
+  methods: {
+    getBeers() {
+      fetch("https://api.punkapi.com/v2/beers")
+        .then((response) => response.json())
+        .then((data) => {
+          this.beers = data;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
