ecff590f15502b76e0023aeb80118671f799f29d
Daniel Rojo <danielrojo@gmail.com>
2022-02-24 16:12:30 +0100

    Beer component added fith range filter

--- src/components/beers/BeersComponent.vue
+++ src/components/beers/BeersComponent.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="container">
+    <vue-slider v-model="range"></vue-slider>
     <div class="row">
       <div
         class="col-sm-12 col-md-6 col-lg-4 my-2"
-        v-for="(beer, index) in beers"
+        v-for="(beer, index) in showBeers"
         :key="index"
       >
         <div class="card h-100">
-          <img class="card-img-top mx-auto mt-2" :src="beer.image_url" alt="" style="width: 60px" />
+          <img
+            class="card-img-top mx-auto mt-2"
+            :src="beer.image_url"
+            alt=""
+            style="width: 60px"
+          />
           <div class="card-body">
             <h4 class="card-title">{{ beer.name }}</h4>
-            <h6 >{{ beer.first_brewed }}</h6>
-            <h6 >{{ beer.abv }}</h6>
+            <h6>{{ beer.first_brewed }}</h6>
+            <h6>{{ beer.abv }}</h6>
             <p class="card-text">{{ beer.tagline }}</p>
           </div>
         </div>
@@ -26,6 +32,7 @@ export default {
   data() {
     return {
       beers: [],
+      range: [3, 5],
     };
   },
   created() {
@@ -40,6 +47,14 @@ export default {
         });
     },
   },
+  computed: {
+    showBeers() {
+      return this.beers.filter(
+        (beer) => beer.abv >= this.range[0] && beer.abv <= this.range[1]
+      ).sort((a, b) => a.abv - b.abv);
+    },
+  },
+  components: {},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import VueYoutube from 'vue-youtube'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// main.js
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
+Vue.component('VueSlider', VueSlider)
  
 Vue.use(VueYoutube)
 Vue.use(BootstrapVue)
